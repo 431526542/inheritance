ae43de1e2a8a3f3cc36053a1768569842a037dbe
kingdaesol <eothf97@gmail.com>
2022-06-09 16:03:11 +0900

    22.06.08

---
+++ Circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"Circle.h"
+
+Circle::Circle():name("circle"), mX(0), mY(0), mradius(0)
+{
+
+}
+
+Circle::~Circle()
+{
+}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"Circle.h"
+#include"coin.h"
+
+
+using namespace std;
+
+int main()
+{
+	Circle circle1;
+	Coin coin1;
+	circle1.printName();
+	coin1.printName();
+}
